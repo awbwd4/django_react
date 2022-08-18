1c44e30cef58978115313011e1fa598da2506ab9
awbwd4 <awbwd4@gmail.com>
2022-08-18 19:59:46 +0900

    22.08.18 django model field, admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'is_public',
+                    'message_length', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']
+
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
+
+    # def is_public(self):
+    #     return "공개   여부"
--- instagram/models.py
+++ instagram/models.py
@@ -3,5 +3,15 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # def message_length(self):
+    #     return len(self.message)
+
+    # message_length.short_description = "메세지 글자수"
+
+    # java의 toString과 동일
+    def __str__(self) -> str:
+        return self.message
