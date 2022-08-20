ff4cc13a4e514182161f4cfc0efc080f42fe83fb
awbwd4 <awbwd4@gmail.com>
2022-08-20 22:29:51 +0900

    22.08.20 searching

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    qs = Post.objects.all()
+    query = request.GET.get('query', '')
+    if query:
+        qs = qs.filter(message__icontains=query)
+    # instagram/templates/instagram/post_list.html
+    return render(request, 'instagram/post_list.html', {
+        'post_list': qs,
+        'query': query
+    })
