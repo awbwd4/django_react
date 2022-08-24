3de48068e8f856eb99dbfcef97ed20ebe78e425f
awbwd4 <awbwd4@gmail.com>
2022-08-24 16:13:46 +0900

    22.08.24 function based view

--- instagram/views.py
+++ instagram/views.py
@@ -1,14 +1,28 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 from .models import Post
 
 
-def post_list(request):
-    qs = Post.objects.all()
-    query = request.GET.get('query', '')
-    if query:
-        qs = qs.filter(message__icontains=query)
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'query': query
-    })
+post_list = ListView.as_view(model=Post)
+
+
+# def post_list(request: HttpResponse):
+#     qs = Post.objects.all()
+#     query = request.GET.get('query', '')
+#     if query:
+#         qs = qs.filter(message__icontains=query)
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'query': query
+#     })
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("hello world")
+    response.write("hello world")
+    response.write("hello world")
+    response.write("hello world")
+    return response
